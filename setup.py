490429cadb1c85c617e1b7fdb219d8d182b3f580
Pior Bastida <pior@pbastida.net>
2016-11-13 12:26:53 -0500

    Setup pypi deployment

--- setup.py
+++ setup.py
@@ -2,20 +2,44 @@ from setuptools import find_packages, setup
 
 dependencies = ['boto3', 'click', 'pyyaml', 'tabulate', 'Arrow']
 
+classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Environment :: Console',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Natural Language :: English',
+    'Operating System :: POSIX :: Linux',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: Implementation :: CPython',
+    'Topic :: System',
+    'Topic :: Software Development',
+]
+
+entry_points = {
+    'console_scripts': [
+        'bcluster = buddy.command.cluster:cli',
+        'bstack = buddy.command.stack:cli',
+    ],
+}
+
+
 setup(
     name='buddy',
     version='0.0.0',
-    url='',
+
     description='Buddy, your Cloudformation/ECS valet',
+    license='MIT License',
+    platforms='Linux',
+    classifiers=classifiers,
+
+    url='https://github.com/pior/buddy',
+    author='Pior Bastida',
+    author_email='pior@pbastida.net',
+
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
     zip_safe=False,
-    platforms='any',
     install_requires=dependencies,
-    entry_points={
-        'console_scripts': [
-            'bcluster = buddy.command.cluster:cli',
-            'bstack = buddy.command.stack:cli',
-        ],
-    },
+    entry_points=entry_points,
 )
