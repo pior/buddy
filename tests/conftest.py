f1ed70ed915d2a5ce65157a1ede79e7b88ef5a6f
Pior Bastida <pior@pbastida.net>
2016-11-13 14:35:14 -0500

    Improve fixtures / mock for AWS cloudformation

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,11 @@
-import pytest
+import os
+import json
+
 from _pytest.monkeypatch import monkeypatch
+from click.testing import CliRunner
+import moto
+import boto3
+import pytest
 
 
 @pytest.fixture(scope="session")
@@ -17,3 +23,46 @@ def aws_config(monkeypatch_session):
         'AWS_SECRET_ACCESS_KEY',
         'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'
     )
+
+
+def assert_if_cwd_not_empty():
+    cwd = os.getcwd()
+    dir_content = os.listdir(cwd)
+    if dir_content:
+        raise Exception('Current working directory not empty:'
+                        ' %s: %s' % (cwd, dir_content))
+
+
+@pytest.fixture
+def runner():
+    cli_runner = CliRunner()
+    with cli_runner.isolated_filesystem():
+        yield cli_runner
+        assert_if_cwd_not_empty()
+
+
+TEST_TEMPLATE = {
+    "AWSTemplateFormatVersion": "2010-09-09",
+    "Resources": {
+        "Sns": {
+            "Type": "AWS::SNS::Topic",
+            "Properties": {
+                "TopicName": "Hello"
+            }
+        }
+    }
+}
+TEST_TEMPLATE_BODY = json.dumps(TEST_TEMPLATE)
+
+
+@pytest.fixture
+def stack():
+    client = boto3.client('cloudformation')
+    client.create_stack(
+        StackName='HelloWorld', TemplateBody=TEST_TEMPLATE_BODY)
+
+
+@pytest.fixture
+def mock_cloudformation():
+    with moto.mock_cloudformation():
+        yield
--- tests/test_command_stack.py
+++ tests/test_command_stack.py
@@ -1,59 +1,12 @@
-import json
-import os
-
-from click.testing import CliRunner
-from moto import mock_cloudformation
-import boto3
-import pytest
-
 from buddy.command.stack import cli
 
 
-TEST_TEMPLATE = {
-    "AWSTemplateFormatVersion": "2010-09-09",
-    "Resources": {
-        "Sns": {
-            "Type": "AWS::SNS::Topic",
-            "Properties": {
-                "TopicName": "Hello"
-            }
-        }
-    }
-}
-TEST_TEMPLATE_BODY = json.dumps(TEST_TEMPLATE)
-
-
-def setup_test_stack():
-    client = boto3.client('cloudformation')
-    client.create_stack(
-        StackName='HelloWorld', TemplateBody=TEST_TEMPLATE_BODY)
-
-
-def assert_if_cwd_not_empty():
-    cwd = os.getcwd()
-    dir_content = os.listdir(cwd)
-    if dir_content:
-        raise Exception('Current working directory not empty:'
-                        ' %s: %s' % (cwd, dir_content))
-
-
-@pytest.fixture
-def runner():
-    cli_runner = CliRunner()
-    with cli_runner.isolated_filesystem():
-        yield cli_runner
-        assert_if_cwd_not_empty()
-
-
-@mock_cloudformation
-def test_list_empty(runner):
+def test_list_empty(mock_cloudformation, runner):
     result = runner.invoke(cli, ['list'])
     assert result.exit_code == 0
 
 
-@mock_cloudformation
-def test_list(runner):
-    setup_test_stack()
+def test_list(mock_cloudformation, runner, stack):
     result = runner.invoke(cli, ['list'])
     assert result.exit_code == 0
     assert 'HelloWorld' in result.output
